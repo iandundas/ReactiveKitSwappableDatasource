70914f08cd38ab7b54deb4f6dce4806602624169
Ian Dundas <contact@iandundas.co.uk>
2016-05-17 19:10:34 +0800

    added xcode project for carthage, & cartfile

---
+++ ReactiveKitSwappableDatasource/ReactiveKitSwappableDatasource.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveKitSwappableDatasource.h
+//  ReactiveKitSwappableDatasource
+//
+//  Created by Ian Dundas on 17/05/2016.
+//  Copyright © 2016 IanDundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveKitSwappableDatasource.
+FOUNDATION_EXPORT double ReactiveKitSwappableDatasourceVersionNumber;
+
+//! Project version string for ReactiveKitSwappableDatasource.
+FOUNDATION_EXPORT const unsigned char ReactiveKitSwappableDatasourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveKitSwappableDatasource/PublicHeader.h>
+
+
