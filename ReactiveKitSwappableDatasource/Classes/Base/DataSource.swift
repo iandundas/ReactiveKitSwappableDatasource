ff7f093305377833efaef2aad96465bd91ddfbe2
Ian Dundas <contact@iandundas.co.uk>
2016-06-05 18:43:24 +0700

    Adding replaceItems() method to ManualDataSource

--- ReactiveKitSwappableDatasource/Classes/Base/DataSource.swift
+++ ReactiveKitSwappableDatasource/Classes/Base/DataSource.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 IanDundas. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import ReactiveKit
 
 public protocol DataSourceType {
@@ -24,13 +24,17 @@ public class ManualDataSource<Item: Equatable>: DataSourceType {
     public func items() -> [Item] {
         return collection.filter{_ in true}
     }
+    
+    // only available publicly on ManualDataSource (because it's.. manual)
+    public func replaceItems(items: [Item]) {
+        collection.replace(items, performDiff: true)
+    }
 
     public func mutations() -> Stream<CollectionChangeset<[Item]>>{
         return collection.filter {_ in true}
     }
 
     private let collection: CollectionProperty<Array<Item>>
-    private let disposeBag = DisposeBag()
 
     /* TODO: currently only takes Array CollectionType
         init<C: CollectionType where C.Generator.Element == Element>(collection: C){
