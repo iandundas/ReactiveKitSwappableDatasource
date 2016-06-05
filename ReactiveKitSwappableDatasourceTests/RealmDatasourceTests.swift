bb4d3146d06e6d83bce4df0dd800f9cdfdaf6882
Ian Dundas <contact@iandundas.co.uk>
2016-06-05 18:57:01 +0700

    Added documenting tests framing the interaction of Realm data source

---
+++ ReactiveKitSwappableDatasourceTests/RealmDatasourceTests.swift
@@ -0,0 +1,63 @@
+//
+//  RealmDatasourceTests.swift
+//  ReactiveKitSwappableDatasource
+//
+//  Created by Ian Dundas on 05/06/2016.
+//  Copyright © 2016 IanDundas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import RealmSwift
+import ReactiveKit
+import Nimble
+
+@testable import ReactiveKitSwappableDatasource
+
+class RealmDatasourceTests: XCTestCase {
+
+    var realm: Realm!
+    var disposeBag: DisposeBag!
+    
+    var datasource: RealmDataSource<Cat>!
+    
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
+        
+        realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: NSUUID().UUIDString))
+        
+        datasource = RealmDataSource<Cat>(collection: realm.objects(Cat))
+    }
+    
+    override func tearDown() {
+        disposeBag.dispose()
+        
+        realm = nil
+        disposeBag = nil
+        datasource = nil
+        
+        super.tearDown()
+    }
+    
+//    func testStartingConditions() {
+//        
+//        expect(self.realm.objects(Cat).count).to(equal(0))
+//        expect(self.datasource.items().count).to(equal(0))
+//    }
+//    
+//    func testInsertMutationWorking(){
+//        var insertions = -1
+//        
+//        datasource.mutations().observeNext({ changeset in
+//            insertions = changeset.inserts.count
+//        }).disposeIn(disposeBag)
+//        
+//        try! realm.write {
+//            realm.add(Cat(value: ["name" : "Mr Catzz"]))
+//            realm.add(Cat(value: ["name" : "Mr Lolz"]))
+//        }
+//        
+//        expect(insertions).toEventually(equal(2), timeout: 3)
+//    }
+}
--- ReactiveKitSwappableDatasourceTests/RealmNotificationTests.swift
+++ ReactiveKitSwappableDatasourceTests/RealmNotificationTests.swift
@@ -1,56 +1,59 @@
-//import UIKit
-//import XCTest
-//import RealmSwift
-//import ReactiveKit
-//import Nimble
-//
-//@testable import ReactiveKitSwappableDatasource
-//
-//
-//class RealmNotificationTests: XCTestCase {
-//    
-//    var realm: Realm!
-//    var disposeBag: DisposeBag!
-//    
-//    var datasource: RealmDataSource<Cat>!
-//    
-//    override func setUp() {
-//        super.setUp()
-//        disposeBag = DisposeBag()
-//        
-//        realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: NSUUID().UUIDString))
-//        
-//        datasource = RealmDataSource<Cat>(collection: realm.objects(Cat))
-//    }
-//    
-//    override func tearDown() {
-//        disposeBag.dispose()
-//        
-//        realm = nil
-//        disposeBag = nil
-//        datasource = nil
-//        
-//        super.tearDown()
-//    }
-//    
-//    func testStartingConditions() {
-//        
-//        expect(self.realm.objects(Cat).count).to(equal(0))
-//        expect(self.datasource.items().count).to(equal(0))
-//    }
-//    
-//    func testInsertMutationWorking(){
-//        var insertions = -1
-//        
-//        datasource.mutations().observeNext({ changeset in
-//            insertions = changeset.inserts.count
-//        }).disposeIn(disposeBag)
-//        
-//        try! realm.write {
-//            realm.add(Cat(value: ["name" : "Mr Catzz"]))
-//            realm.add(Cat(value: ["name" : "Mr Lolz"]))
-//        }
-//        
-//        expect(insertions).toEventually(equal(2), timeout: 3)
-//    }
-//}
+import UIKit
+import XCTest
+import RealmSwift
+import ReactiveKit
+import Nimble
+
+@testable import ReactiveKitSwappableDatasource
+
+/*  
+    Realm should provide to us an initial notification containing the fetched results, and then
+    provide updates afterwards about any changes.
+*/
+
+class RealmNotificationTests: XCTestCase {
+    
+    var realm: Realm!
+    var bag = DisposeBag()
+    
+    override func setUp() {
+        super.setUp()
+        
+        realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: NSUUID().UUIDString))
+    }
+    
+    override func tearDown() {
+        bag.dispose()
+        realm = nil
+        
+        super.tearDown()
+    }
+    
+    func testStartingConditions() {
+        expect(self.realm.objects(Cat).count).to(equal(0))
+    }
+    
+    func testInsertNotificationWorking(){
+        var insertions = 0
+        
+        let token = realm.objects(Cat).addNotificationBlock { (changeSet:RealmCollectionChange) in
+            switch changeSet {
+            case .Initial(let cats):
+                insertions += cats.count
+            case .Update(_):
+                fail("Update should never be called")
+            case .Error:
+                fail("Error should never be called")
+            }
+        }
+
+        bag.addDisposable(BlockDisposable{token.stop()})
+        
+        try! realm.write {
+            realm.add(Cat(value: ["name" : "Mr Catzz"]))
+            realm.add(Cat(value: ["name" : "Mr Lolz"]))
+        }
+        
+        expect(insertions).toEventually(equal(2), timeout: 3)
+    }
+}
